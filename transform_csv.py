84e4b3476baed45a46cd6190f99280c97c889a2c
conrad94 <94002787@student.wintec.ac.nz>
2025-02-15 11:35:06 +1300

    Update transform_csv.py

--- transform_csv.py
+++ transform_csv.py
@@ -55,7 +55,7 @@ all_data = pd.DataFrame()  # Empty DataFrame to hold all the merged data
 for file_name in os.listdir(data_dir):
     if file_name.lower().endswith(".csv"):  # This will handle both .csv and .CSV extensions
         file_path = os.path.join(data_dir, file_name)
-        df = pd.read_csv(file_path,skiprows=5, quotechar='"', skipfooter=3, engine="python")  # Read the CSV file
+        df = pd.read_csv(file_path,skiprows=6, quotechar='"', skipfooter=3, engine="python")  # Read the CSV file
         # df.columns = df.columns.str.strip()  # Remove extra spaces in column names
         transformed_df = transform_data(df)  # Apply transformations
         all_data = pd.concat([all_data, transformed_df], ignore_index=True)  # Append the data
